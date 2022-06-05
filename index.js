36693a95ac1c46f30e7655b5a076343675ca292f
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 16:33:51 +1000

    13 Passing tests

--- index.js
+++ index.js
@@ -29,9 +29,38 @@ function distanceTravelledInFeet(start, end){
     }
 }
 
-
+// 10-13 tests
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end)
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+        
+}
 
 
 /*
+  describe('calculatesFarePrice(start, destination)', function() {
+    it('gives customers a free sample', function() {
+      expect(calculatesFarePrice(43, 44)).to.equal(0);
+    });
 
+    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
+      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
+      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
+    });
+
+    it('charges 25 dollars for a distance over 2000 feet', function() {
+      expect(calculatesFarePrice(50, 58)).to.equal(25);
+    });
+
+    it('does not allow rides over 2500 feet', function() {
+      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+    });
 */
